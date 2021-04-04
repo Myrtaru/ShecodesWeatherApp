b11de476517bfc53124beb2f82905b6286791166
Myrta R <80482586+Myrtaru@users.noreply.github.com>
2021-04-04 19:00:25 +0200

    updated some functions

--- src/index.js
+++ src/index.js
@@ -21,35 +21,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function showTemperature(response) {
+  celsiusTemp = response.data.main.temp;
   document.querySelector("#dayTemp").innerHTML = Math.round(celsiusTemp);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
-    3.6 * response.data.wind.speed
-  );
+    3.6 * response.data.wind.speed);
   document.querySelector("#date").innerHTML = formatDate(
-    response.data.dt * 1000
-  );
+    response.data.dt * 1000);
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute(
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
-    iconElement.setAttribute("alt", response.data.weather[0].description); 
-    celsiusTemp = response.data.main.temp;
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchCity(city) {
   let apiKey = "d161f604274c06b1e5ec41b1728c9abc";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
-function search(event) {
+function submitSearch(event) {
   event.preventDefault();
-let searchInput = document.querySelector("#search-input").value;
+let searchInput = document.querySelector("#search-input");
 searchCity(searchInput.value);
-searchCity(searchInput);
 } 
 function showFahrenheitTemp(event){
   event.preventDefault();
@@ -57,18 +54,16 @@ function showFahrenheitTemp(event){
   fahrenheitLink.classList.add("active");
   document.querySelector("#dayTemp").innerHTML = Math.round((celsiusTemp * 9) / 5 + 32);
 }
-
 function showCelsiusTemp(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
   document.querySelector("#dayTemp").innerHTML = Math.round(celsiusTemp);
 }
-
 let celsiusTemp = null;
 
 let formInput = document.querySelector("#search-form");
-formInput.addEventListener("submit", search);
+formInput.addEventListener("submit", submitSearch);
 
 let fahrenheitLink = document.querySelector("#linkFahrenheitDay");
 fahrenheitLink.addEventListener("click", showFahrenheitTemp);
